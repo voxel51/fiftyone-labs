eed5f4afc5bca5d464b3b79e73c412ccf26b6d08
Neeraja Abhyankar <neeraja.abhyankar@gmail.com>
2026-04-22 15:59:45 -0500

    feat: compatibility with fiftyone v1.14.1

--- plugins/label_propagation/sam2_local.py
+++ plugins/label_propagation/sam2_local.py
@@ -191,13 +191,13 @@ class SegmentAnything2VideoModel(FiftyOneSegmentAnything2VideoModel):
         if self.ctx is not None:
             with self.ctx:
                 model = entrypoint(
-                    config.entrypoint_args["model_cfg"],
+                    config.entrypoint_args["config_file"],
                     ckpt_path=config.model_path,
                     device=self._device,
                 )
         else:
             model = entrypoint(
-                config.entrypoint_args["model_cfg"],
+                config.entrypoint_args["config_file"],
                 ckpt_path=config.model_path,
                 device=self._device,
             )
@@ -296,13 +296,7 @@ class SegmentAnything2VideoModel(FiftyOneSegmentAnything2VideoModel):
                     "'prompt_field' should be a frame field for segment anything 2 video model"
                 )
 
-        negative_prompt_field = None
-
-        # TODO(neeraja): remove negative prompt field from the return value
-        # once fiftyone/fiftyone/utils/sam2.py::SegmentAnything2VideoModel::predict
-        # is edited to not use negative prompts
-        # until then, the version is capped in fiftyone.yml
-        return prompt_field, negative_prompt_field
+        return prompt_field
 
     def _get_prompt_type(self, sample, field_name):
         for _, frame in sample.frames.items():
